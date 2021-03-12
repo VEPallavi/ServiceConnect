61a87404f95323b1a2eacd903213acaa24d16f89
pallavi sinha <pallavisinha@virtualemployee.com>
2021-03-12 20:02:43 +0530

    commit 12-03-2021 customer

--- app/src/main/java/com/Servicehubconnect/activity/customer/SettingActivity.kt
+++ app/src/main/java/com/Servicehubconnect/activity/customer/SettingActivity.kt
@@ -22,6 +22,7 @@ class SettingActivity : AppCompatActivity(), View.OnClickListener{
     var viewModel: SettingViewModel?= null
 
 
+    
 
 
     override fun onCreate(savedInstanceState: Bundle?) {
